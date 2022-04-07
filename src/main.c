4d187b7ff06eede2dab5c09d390e7e5005c0fc2a
larvi jerome <jerome_larvi@ecole2600.com>
2022-04-07 17:41:57 -0400

    test in progress2

--- src/main.c
+++ src/main.c
@@ -2,12 +2,12 @@
 #include "my_secmalloc.c"
 #include "../include/my_secmalloc.h"
 #include <sys/mman.h>
-#include <string.h> // to remove after tests.
 
 int main(){
     size_t size = 1078;
+
     int *pointer = my_malloc(size);
-    int *pointeur2 =  my_calloc(20, 40);
+    int *pointer2 =  my_calloc(20, 40);
     int *pointer3 = my_realloc(pointer,45);
     my_free(pointer);
     
