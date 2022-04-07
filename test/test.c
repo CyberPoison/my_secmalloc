f26e25658c01b7997aacd0f18c958bebd354cda7
root <davidof130@gmail.com>
2022-04-07 05:20:34 -0400

    Update

--- test/test.c
+++ test/test.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <criterion/criterion.h>
 #include <../src/my_secmalloc.c>
+#include <signal.h>
 
 void setup(void) {
     puts("Runs before the test");
 }
 
-test(){
-    size_t size = 1078;
-    int *pointer = my_malloc(size);
+Test(sample, passing, .signal = SIGSEGV){
+    int *pointer = my_malloc(1080);
     my_free(pointer);
 }
 
