cc2762ed5e246b5e8e8595eb7b3da71f2b1410ce
root <davidof130@gmail.com>
2022-04-08 08:09:46 -0400

    Update files

--- src/my_secmalloc.c
+++ src/my_secmalloc.c
@@ -1,23 +1,54 @@
 #include <stdio.h>
-#include "../include/my_secmalloc.h"
+#include <stdlib.h>
 #include <sys/mman.h>
-#include <string.h> // to remove after tests.
+#include <string.h>
+#include <time.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+
+FILE *inicial_log(){
+    FILE *fp;
+    char *path = getenv("MSM_OUPUT");
+    if(path == NULL){
+        path = "./root/ecole2600/my_secmallocgit/Log.txt";
+    }
+    fp = fopen(path,"a+");
+    time_t timestamp = time( NULL );
+    struct tm * timeInfos = localtime( & timestamp );
+    log_generator(fp,"*******************\n");
+    //log_generator(fp,("%02d:%02d:%02d\n\n",timeInfos->tm_hour, timeInfos->tm_min,timeInfos->tm_sec));
+    return fp;
+}
+
+void log_generator(int fd, const char* format,...){
+    char buf [4096];
+    va_list argslist;
+    va_start(argslist,format);
+    int size = vsnprintf(buf,4096,format,argslist);
+    va_end(argslist);
+    write(fd,buf,size);
+
+}
 
 void *my_malloc(size_t size){
-    printf("INFO: Calling malloc function...\n");
+    FILE *fp = inicial_log();
+
+    log_generator(fp,"INFO: Calling malloc function...\n");
     if(size == 0){
-        printf("WARN: allocating 0 bytes returning NULL pointer\n"); 
+        log_generator(fp,"WARN: allocating 0 bytes returning NULL pointer\n"); 
         return NULL;
     }
     int *pointer = mmap(NULL,sizeof(size_t)+size,PROT_READ | PROT_WRITE | PROT_EXEC,MAP_PRIVATE | MAP_ANONYMOUS,-1,0);
     if(pointer == MAP_FAILED) {
-        printf("ERROR: Mapping Failed\n");  
+        log_generator(fp,"ERROR: Mapping Failed\n");  
     }
     else {
         //int *ptr = pointer;
         pointer += sizeof(size_t)/sizeof(int); // 8/4 = 2
         //*ptr = size + sizeof(size_t);
-        printf("INFO: Successfully Allocate %ld bytes at %p\n",size,pointer);
+        log_generator(fp,"INFO: Successfully Allocate %ld bytes at %p\n",size,pointer);
     }
     
     return pointer;
@@ -25,42 +56,43 @@ void *my_malloc(size_t size){
 
 
 void my_free(void *ptr){
-    printf("INFO: Calling free function...\n");
+    FILE *fp = inicial_log();
+    log_generator(fp,"INFO: Calling free function...\n");
     if(ptr != NULL){
         int *pointer = ptr - sizeof(size_t); 
         int size = *pointer;
         int result = munmap(pointer,size);
         if(0 == result){
-            printf("INFO: Successfully free %ld bytes at %p",size - sizeof(size_t),ptr);
+            log_generator(fp,"INFO: Successfully free %ld bytes at %p",size - sizeof(size_t),ptr);
         }
     }else{
-       printf("WARN: Performing free on NULL pointer"); 
+       log_generator(fp,"WARN: Performing free on NULL pointer"); 
     }
 }
 
 void *my_calloc(size_t nmemb, size_t size){
-
-    printf("INFO: Calling function calloc...\n");
+    FILE *fp = inicial_log();
+    log_generator(fp,"INFO: Calling function calloc...\n");
     size_t max_size = nmemb * size;
     int *ptr = my_malloc(max_size);
     int *cp_ptr = ptr;
 
-    printf("BEFORE %p\n",ptr);
+    log_generator(fp,"BEFORE %p\n",ptr);
     if(ptr != MAP_FAILED){
         for(int i = 0;i<max_size;i++){
             cp_ptr += 1;
             *cp_ptr = 0;
-            printf("%d",*cp_ptr);
+            log_generator(fp,"%d",*cp_ptr);
         }
-        printf("AFTER %p\n",ptr);
+        log_generator(fp,"AFTER %p\n",ptr);
     }
 
     return ptr;
 }
 
 void *my_realloc(void *ptr, size_t size){
-
-    printf("INFO: Calling function realloc...\n");
+    FILE *fp = inicial_log();
+    log_generator(fp,"INFO: Calling function realloc...\n");
 
     if(ptr == NULL){
         return my_malloc(size);
@@ -74,8 +106,8 @@ void *my_realloc(void *ptr, size_t size){
     int old_size = *pointer - sizeof(size_t);
     *pointer = size + sizeof(size_t);
     if(size > old_size){
-        printf("WARN: size provided bigger than old size, can't reallocate\n");
+        log_generator(fp,"WARN: size provided bigger than old size, can't reallocate\n");
     }
-    printf("INFO: Successfully reallocate %ld bytes at %p \n",size,ptr);
+    log_generator(fp,"INFO: Successfully reallocate %ld bytes at %p \n",size,ptr);
     return ptr;
 }
